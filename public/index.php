00431be30c06f97ce23385ede7b875ee6b2eabbb
chriszarate <chris@zarate.org>
2014-11-22 18:01:51 -0500

    Remove analytics.

--- public/index.php
+++ public/index.php
@@ -32,16 +32,6 @@
       <script src="//cdnjs.cloudflare.com/ajax/libs/respond.js/1.4.2/respond.js"></script>
     <![endif]-->
 
-    <!-- Google Analytics -->
-    <script>
-      (function(b,o,i,l,e,r){b.GoogleAnalyticsObject=l;b[l]||(b[l]=
-      function(){(b[l].q=b[l].q||[]).push(arguments)});b[l].l=+new Date;
-      e=o.createElement(i);r=o.getElementsByTagName(i)[0];
-      e.src='//www.google-analytics.com/analytics.js';
-      r.parentNode.insertBefore(e,r)}(window,document,'script','ga'));
-      ga('create','UA-XXXXXXXXX');ga('send','pageview');
-    </script>
-
   </head>
 
   <body>
