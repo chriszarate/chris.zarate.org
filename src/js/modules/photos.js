1a78cc41f20b7144df5e2e594f7df56d291cdee6
chriszarate <chris@zarate.org>
2016-02-08 23:01:07 -0500

    Create convenience container for photos.

--- src/js/modules/photos.js
+++ src/js/modules/photos.js
@@ -8,9 +8,9 @@ var xmlhttp = new XMLHttpRequest();
 
 xmlhttp.onreadystatechange = function () {
   if (xmlhttp.readyState === 4 && xmlhttp.status === 200) {
-    document.getElementById('photos').innerHTML = xmlhttp.responseText;
+    document.getElementById('photo-container').innerHTML = xmlhttp.responseText;
   }
-}
+};
 
 xmlhttp.open('GET', '/cache/blog.html', true);
 xmlhttp.send();
