bd8ba012e555740f1fa63ab615c295a312d17d81
chriszarate <chris@zarate.org>
2016-05-02 00:22:19 -0400

    Zabriskie update.

--- src/js/main.js
+++ src/js/main.js
@@ -1,9 +1,4 @@
 'use strict';
 
 require('./modules/user-agent.js');
-require('./modules/fake-blog.js');
-require('./modules/masthead.js');
-require('./modules/photos.js');
-
-// Smart underlining
-require('smart-underline').init({});
+require('./modules/glitch.js');
